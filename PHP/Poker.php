060d233cee1d5b09ee98b6a4c30077f31fab5086
BlueSky_07 <bluesky_07@qq.com>
2017-03-20 12:01:31 +0800

    PHP version

---
+++ PHP/Poker.php
@@ -0,0 +1,207 @@
+<?php
+$err = [
+    'errCode' => 400,
+    'errText' => 'unknown error',
+];
+if (empty($_SERVER['QUERY_STRING'])) {
+    $err['errCode'] = 401;
+    $err['errText'] = 'empty pokers';
+    exitCode($err);
+} else {
+    $in = urldecode($_SERVER['QUERY_STRING']);
+    $in = htmlspecialchars_decode($in);
+}
+$pokersArr = gameIn($in);
+/*{
+    // for debug
+    $sum = getPokersSum($pokersArr);
+    echo $sum;
+    var_dump($pokersArr);
+}*/
+judge($pokersArr);
+exitCode($err);
+
+function gameIn($in)
+{
+    // change to upper case
+    $in = strtoupper($in);
+    // delete wrong characters
+    $in = preg_replace('/[^\dJQKA]/', '', $in);
+    // take the first 5
+    $in = substr($in, 0, 5);
+    // less than 5 is not right
+    if (strlen($in) < 5) {
+        $err['errCode'] = 402;
+        $err['errText'] = 'not enough pokers';
+        exitCode($err);
+    }
+    // change to array
+    $in = str_split($in);
+    // get count of each poker
+    $pokersArr = [];
+    foreach ($in as $item) {
+        if (!array_key_exists($item, $pokersArr)) {
+            $pokersArr[$item] = 1;
+        } else {
+            $pokersArr[$item]++;
+        }
+    }
+    // change 1 to A
+    if (array_key_exists(1, $pokersArr)) {
+        if (array_key_exists('A', $pokersArr)) {
+            $pokersArr['A'] += $pokersArr[1];
+        } else {
+            $pokersArr['A'] = $pokersArr[1];
+        }
+        unset($pokersArr[1]);
+    }
+    // change 0 to 10
+    if (array_key_exists(0, $pokersArr)) {
+        $pokersArr[10] = $pokersArr[0];
+        unset($pokersArr[0]);
+    }
+    return $pokersArr;
+}
+
+function judge($pokersArr)
+{
+    judgeBigBouns($pokersArr);
+    judgeOtherBouns($pokersArr);
+}
+
+function judgeBigBouns($pokersArr)
+{
+    $err['errCode'] = 0;
+    $err['errText'] = '';
+    // bomb
+    $bomb = array_search(4, $pokersArr);
+    if ($bomb) {
+        $err['judgeText'] = [
+            'type' => 'BOMB',
+            'value' => $bomb,
+        ];
+        exitCode($err);
+    }
+    // get sum of pokers
+    $sum = getPokersSum($pokersArr);
+    // five bulls
+    if ($sum == 50) {
+        $err['judgeText'] = [
+            'type' => 'FIVE BULLS',
+        ];
+        exitCode($err);
+    }
+    // five small bulls
+    if ($sum == 10) {
+        $err['judgeText'] = [
+            'type' => 'FIVE SMALL BULLS',
+        ];
+        exitCode($err);
+    }
+}
+
+function judgeOtherBouns($pokersArr)
+{
+    $sum = getPokersSum($pokersArr);
+    $value = $sum % 10 == 0 ? 10 : $sum % 10;
+    $arr2Calc = [];
+    $arr2Show = [];
+    $flag = 0;
+    foreach ($pokersArr as $poker => $pokerCount) {
+        while ($pokerCount > 0) {
+            $pokerCount -= 1;
+            $arr2Calc[$flag] = getPokerVal($poker);
+            $arr2Show[$flag] = $poker;
+            $flag++;
+        }
+    }
+    if (judge10($arr2Calc, 0, 1, 2)) {
+        showBull($arr2Show, $value, 0, 1, 2);
+    } else if (judge10($arr2Calc, 0, 1, 3)) {
+        showBull($arr2Show, $value, 0, 1, 3);
+    } else if (judge10($arr2Calc, 0, 1, 4)) {
+        showBull($arr2Show, $value, 0, 1, 4);
+    } else if (judge10($arr2Calc, 0, 2, 3)) {
+        showBull($arr2Show, $value, 0, 2, 3);
+    } else if (judge10($arr2Calc, 0, 2, 4)) {
+        showBull($arr2Show, $value, 0, 2, 4);
+    } else if (judge10($arr2Calc, 0, 3, 4)) {
+        showBull($arr2Show, $value, 0, 3, 4);
+    } else if (judge10($arr2Calc, 1, 2, 3)) {
+        showBull($arr2Show, $value, 1, 2, 3);
+    } else if (judge10($arr2Calc, 1, 2, 4)) {
+        showBull($arr2Show, $value, 1, 2, 4);
+    } else if (judge10($arr2Calc, 1, 3, 4)) {
+        showBull($arr2Show, $value, 1, 3, 4);
+    } else if (judge10($arr2Calc, 2, 3, 4)) {
+        showBull($arr2Show, $value, 2, 3, 4);
+    } else {
+        $err['errCode'] = 0;
+        $err['errText'] = '';
+        $err['judgeText'] = [
+            'type' => 'LOSE',
+        ];
+        exitCode($err);
+    }
+}
+
+function getPokersSum($pokersArr)
+{
+    $sum = 0;
+    foreach ($pokersArr as $poker => $pokerCount) {
+        $sum += $pokerCount * getPokerVal($poker);
+    }
+    return $sum;
+}
+
+function getPokerVal($poker)
+{
+    if ($poker == 'A') {
+        return 1;
+    } else if (in_array($poker, ['J', 'Q', 'K'])) {
+        return 10;
+    } else {
+        return $poker;
+    }
+}
+
+function judge10($arr2Calc, $a, $b, $c)
+{
+    return ($arr2Calc[$a] + $arr2Calc[$b] + $arr2Calc[$c]) % 10 == 0 ? true : false;
+}
+
+function showBull($arr2Show, $value, $a, $b, $c)
+{
+    $err['errCode'] = 0;
+    $err['errText'] = '';
+    $err['judgeText'] = [
+        'type' => 'BULLS',
+        'value' => $value,
+    ];
+    $flag_arr2toSum = 0;
+    $flag_arr2toLeft = 0;
+    $arr2toSum = [];
+    $arr2toLeft = [];
+    for ($i = 0; $i < 5; $i++) {
+        if ($i == $a || $i == $b || $i == $c) {
+            $arr2toSum[$flag_arr2toSum] = $arr2Show[$i];
+            $flag_arr2toSum++;
+            continue;
+        } else {
+            $arr2toLeft[$flag_arr2toLeft] = $arr2Show[$i];
+            $flag_arr2toLeft++;
+            continue;
+        }
+    }
+    $err['judgeText']['toSum'] = $arr2toSum;
+    $err['judgeText']['toLeft'] = $arr2toLeft;
+    exitCode($err);
+}
+
+function exitCode($err)
+{
+    echo json_encode($err);
+    exit();
+}
+
+?>
--- README.md
+++ README.md
@@ -18,7 +18,9 @@ Judge | Example
 没牛 | ( 3 5 7 1 9 )
 
 ## C 版本
+
 #### 运行：
+
 ```
 Welcome to the game:
 what pokers ?
@@ -37,13 +39,53 @@ what pokers ?
 Jqa13
 --------- LOSE ---------
 ```
-#### 输入要求
-输入参数间可加 空格 / Tab / 换行
 
-*A* / *J* / *Q* / *K* 不区分大小写
+#### 输入要求
+
+输入仅识别数字和 *a* / *j* / *q* / *k* / *A* / *J* / *Q* / *K*，其余字符不作处理
 
 *1* 看做 *A*, *0* 看做 *10*
 
 一次输入超过5个一行以前5个做计算
 
 输入不符要求的字符需重新输入5张牌
+
+#### DEMO
+
+*C/Poker_C.exe* (Windows 10 x64)
+
+## PHP 版本
+
+#### 用法
+
+GET *http(s)://your.domain.com/Poker.php?{参数}*
+
+#### JSON 返回值
+
+errCode | errText | judgeText
+--- | --- | ---
+400 | "unknown error" | NULL
+401 | "empty pokers" | NULL
+402 | "not enough pokers" | NULL
+0 | NULL | *judgeText_JSON*
+
+#### judgeText JSON 返回值
+
+type | value | toSum | toLeft
+--- | --- | --- | ---
+"BOMB" | which four-of-a-kind | NULL | NULL
+"FIVE BULLS" | NULL | NULL | NULL | NULL
+"FIVE SMALL BULLS" | NULL | NULL | NULL | NULL
+"BULLS" | value of bull | 3 of pokers to sum | 2 of pokers then left
+
+#### 参数要求
+
+输入仅识别数字和 *a* / *j* / *q* / *k* / *A* / *J* / *Q* / *K*，其余字符不作处理
+
+*1* 看做 *A*, *0* 看做 *10*
+
+超过5个参数以前5个做计算
+
+#### DEMO
+
+*PHP/Poker.htm*
